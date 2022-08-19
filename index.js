64b0af310413d5f0ae608c21cdbe0e2cb9d62e46
Justin White <justinnwhitee@gmail.com>
2022-08-19 01:14:49 -0400

    commit tree 21071050b9495f926fae3e39fda8ebfdd00da983

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solutions in this file
+// for (let age = 30; age < 40; age++){
+//    console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//    for (let i = 0; i < gifts.length; i++) {
+//       console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//       debugger;
+//    }
+
+//    return gifts;
+// }
+
+// wrapGifts(gifts);
+
+const messages = [];
+
+function writeCards(arrayOfStringNames, eventName){
+
+   for(let i = 0; i < arrayOfStringNames.length; i++){
+      messages.push(`Thank you, ${arrayOfStringNames[i]}, for the wonderful ${eventName} gift!`);
+   }
+
+   return messages;
+}
+
+function countDown(n){
+   while(n >= 0) {
+      console.log(n);
+      n--;
+      debugger;
+   }
+}
